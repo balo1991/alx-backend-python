60ede94d2ba8d949b6c72760ca78dc1dc0401f92
khayfire <kayode354@gmail.com>
2025-09-06 16:02:02 +0100

    alx_prodev created

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
 
+seed = __import__('seed')
+
+
+def stream_users():
+    connection = seed.connect_to_prodev()
+
+    if connection:
+
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM user_data;")
+        rows = cursor.fetchall()
+        num=0
+        while num < len(rows):
+            yield rows[num]
+            num += 1
+        cursor.close()
