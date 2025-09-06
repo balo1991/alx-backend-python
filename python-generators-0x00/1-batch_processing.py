18511114e0f822198bb7f13b37a0f5ad2348e105
khayfire <kayode354@gmail.com>
2025-09-06 16:59:51 +0100

    table in rows

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
 
+seed = __import__('seed')
+
+def stream_users_in_batches(connection, batch_size=10):
+
+    
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data;")
+
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+    cursor.close()
+    
+def batch_processing(connection, batch_size=10):
+    for batch in stream_users_in_batches(connection, batch_size):  # loop 2
+        yield [user for user in batch if int(user["age"]) > 25]   # comprehension (loop 3)
