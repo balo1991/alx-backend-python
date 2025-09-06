92e8fdf39d424b17014d6ee515d5dada338c4dc4
khayfire <kayode354@gmail.com>
2025-09-07 00:37:13 +0100

    average age

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 seed = __import__('seed')
 
-def streamuserages(connection):
-    """
-    Generator that yields user ages one by one from user_data.
-    """
-    cursor = connection.cursor()
-    cursor.execute("SELECT age FROM user_data;")
-    for (age,) in cursor:   # ✅ yields one value at a time
-        yield int(age)
-    cursor.close()
+
     
 def stream_user_ages(connection):
     """
@@ -27,7 +19,7 @@ def average_age(connection):
     """
     total = 0
     count = 0
-    for age in streamuserages(connection):  # ✅ streaming
+    for age in stream_user_ages(connection):  # ✅ streaming
         total += age
         count += 1
     return total / count if count > 0 else 0
